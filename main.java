6270ea9baf5ecc6ba81f357042b9bd2706bab067
leena3304 <146821102+leena3304@users.noreply.github.com>
2025-02-21 14:21:08 +0530

    Create main.java

---
+++ main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+       /* pizza basepizza=new pizza(true);
+        basepizza.addExtraCheese();
+        basepizza.addExtraToppings();
+        basepizza.takeAway();
+        basepizza.getBill();*/
+        Deluxpizza dp=new Deluxpizza(false);
+        dp.getBill();
+
+    }
+}
